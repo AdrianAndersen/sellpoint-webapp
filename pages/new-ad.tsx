aa43ad08f17a3c0b8324ae7d25b03899ee51df87
AdrianAndersen <adrianandersen@protonmail.com>
2021-02-18 11:35:09 +0100

    Create placeholder pages

---
+++ pages/new-ad.tsx
@@ -0,0 +1,12 @@
+import Typography from "@material-ui/core/Typography";
+const CreateAdvertisementPage = () => {
+  return (
+    <div className="w-full h-5/6 bg-pink-400 p-10 text-center">
+      <Typography variant="h6" className="h-full flex flex-col justify-center">
+        Create Advertisement Component
+      </Typography>
+    </div>
+  );
+};
+
+export default CreateAdvertisementPage;
---
+++ pages/new-listing.tsx
@@ -0,0 +1,12 @@
+import Typography from "@material-ui/core/Typography";
+const CreateListingPage = () => {
+  return (
+    <div className="w-full h-5/6 bg-blue-500 p-10 text-center">
+      <Typography variant="h6" className="h-full flex flex-col justify-center">
+        New Listing Component
+      </Typography>
+    </div>
+  );
+};
+
+export default CreateListingPage;
