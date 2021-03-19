0387f53e52e7086a7f18bc0311edc0e4a340f1f0
AdrianAndersen <adrianandersen@protonmail.com>
2021-03-19 15:58:02 +0100

    Add database tool page

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,13 +1,9 @@
-import { Button, Card, Typography } from "@material-ui/core";
+import { Card, Typography } from "@material-ui/core";
 import Head from "next/head";
 import CreateNewCategory from "../components/Listing/CreateNewCategory";
-import { useGlobalState } from "../components/StateManagement/GlobalStateProvider";
 import UserManager from "../components/User/UserManager";
-import populateDB, { emptyDB } from "../lib/databaseUtil";
-import { demoData, testData } from "../lib/fixtures";
 
 const AdminPage = () => {
-  const { state } = useGlobalState();
   return (
     <>
       <Head>
@@ -20,31 +16,6 @@ const AdminPage = () => {
         <div className="p-4">
           <UserManager />
           <CreateNewCategory />
-          {state.usingDB && (
-            <>
-              <Button
-                onClick={() => {
-                  emptyDB();
-                }}
-              >
-                Tøm databasen
-              </Button>
-              <Button
-                onClick={() => {
-                  populateDB(testData);
-                }}
-              >
-                Last inn test-data
-              </Button>
-              <Button
-                onClick={() => {
-                  populateDB(demoData);
-                }}
-              >
-                Last inn demo-data
-              </Button>
-            </>
-          )}
         </div>
       </Card>
     </>
