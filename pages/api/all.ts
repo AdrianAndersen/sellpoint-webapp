77613cc0c9c87b6ed76e1abc7a39713b3f5e8c17
AdrianAndersen <adrianandersen@protonmail.com>
2021-03-20 17:20:42 +0100

    Combine multiple db requests for faster load times

--- pages/api/all.ts
+++ pages/api/all.ts
@@ -1,12 +1,5 @@
-import { Decimal } from "@prisma/client/runtime";
 import type { NextApiRequest, NextApiResponse } from "next";
-import {
-  Advertisement,
-  GlobalState,
-  LatLng,
-  Listing,
-  User,
-} from "../../lib/Types";
+import { GlobalState, User } from "../../lib/Types";
 import prisma from "../../lib/prisma";
 import { testData } from "../../lib/fixtures";
 
@@ -15,45 +8,55 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     res.status(200);
     res.json(testData);
   } else {
-    const userData = await prisma.user.findMany();
-    const listingData = await prisma.listing.findMany({
-      include: { categories: true },
-    });
     const categoryData = await prisma.category.findMany();
-    const advertisementData = await prisma.advertisement.findMany();
+    const userData = await prisma.user.findMany({
+      include: {
+        listings: {
+          include: {
+            categories: true,
+          },
+        },
+        advertisements: true,
+      },
+    });
+
+    const listings: any[] = [];
+    const advertisements: any[] = [];
 
     const users = userData.map((user) => {
-      const userObj: { lat?: Decimal; lng?: Decimal; location: LatLng } = {
+      const userObj: any = {
         ...user,
         location: { lat: Number(user.lat), lng: Number(user.lng) },
       };
+      delete userObj.listings;
+      delete userObj.advertisements;
       delete userObj.lat;
       delete userObj.lng;
+
+      user.listings.forEach((listing: any) => {
+        const listingObj = {
+          ...listing,
+          categories: (listing.categories || []).map(
+            (cat: { name: string }) => cat.name
+          ),
+          owner: listing.ownerId,
+        };
+        delete listingObj.ownerId;
+        listings.push(listingObj);
+      });
+
+      user.advertisements.forEach((ad: any) => {
+        const advertisementObj = {
+          ...ad,
+          owner: ad.ownerId,
+        };
+        delete advertisementObj.ownerId;
+        advertisements.push(advertisementObj);
+      });
       return userObj as User;
     });
 
-    const listings = listingData.map((listing: any) => {
-      const listingObj = {
-        ...listing,
-        categories: (listing.categories || []).map(
-          (cat: { name: string }) => cat.name
-        ),
-        owner: listing.ownerId,
-      };
-      delete listingObj.ownerId;
-      return listingObj as Listing;
-    });
-
     const categories = categoryData.map((category) => category.name);
-
-    const advertisements = advertisementData.map((advertisement: any) => {
-      const advertisementObj = {
-        ...advertisement,
-        owner: advertisement.ownerId,
-      };
-      delete advertisementObj.ownerId;
-      return advertisementObj as Advertisement;
-    });
 
     const globalState: GlobalState = {
       currentUser: undefined,
